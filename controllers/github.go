1264245059b010760e44b00f5204203e42af342f
Shawn <wangxiaofan0529@gmail.com>
2016-05-14 12:38:12 +0800

    webhook

--- controllers/github.go
+++ controllers/github.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/astaxie/beego"
 	"mdblog/models"
+	"os/exec"
 )
 
 type GithubController struct {
@@ -12,6 +15,23 @@ type GithubController struct {
 // @router / [post]
 func (o *GithubController) Post() {
 	isValid := models.ValidateSignature(o.Ctx.Input.Header("X-Hub-Signature"), string(o.Ctx.Input.RequestBody))
-	o.Data["json"] = map[string]bool{"ObjectId": isValid}
+	isPush := o.Ctx.Input.Header("X-GitHub-Event") == "push"
+	// we only listen push event
+	if isValid && isPush {
+		var req GitHubRequest
+		json.Unmarshal(o.Ctx.Input.RequestBody, &req)
+		ref := "refs/heads/" + beego.AppConfig.String("githubbranch")
+		if req.Ref == ref {
+			var cmd = exec.Command("bash", "-c", "cd "+beego.AppConfig.String("localfolder")+"&& git pull")
+			err := cmd.Run()
+			fmt.Println(err)
+		}
+	}
+	// just return
 	o.ServeJSON()
+}
+
+// only need ref parameter to see if is master branch
+type GitHubRequest struct {
+	Ref string
 }
